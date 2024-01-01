2789d5896b702c9a4069d121cc3f519c9b4a4766
obehi234 <obehi.ataga234@gmail.com>
2024-01-02 00:25:55 +0100

    Set up magic ball 8 app functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,53 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  runApp(const MaterialApp());
+  runApp(const MaterialApp(
+    home: BallPage(),
+  ));
 }
+
+class BallPage extends StatelessWidget {
+  const BallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: const Text('Ask Me Anything'),
+          backgroundColor: Colors.blue.shade900,
+        ),
+        body: const Ball()
+      )
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(onPressed: () {
+      setState(() {
+        ballNumber = Random().nextInt(5) + 1;
+        print('I got clicked- $ballNumber');
+      });
+
+    },
+    child:  Center(child: Image(image: AssetImage('images/ball$ballNumber.png'))));
+  }
+}
+
 
 
 
